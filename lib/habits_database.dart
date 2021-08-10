0589d8ea5a9169ac72d943d8ffdd637d08243d74
SHAMOL <shamoldas1997@gmail.com>
2021-08-10 13:02:41 +0600

    Habits_Alarming app push notification depend on user settings times

--- lib/habits_database.dart
+++ lib/habits_database.dart
@@ -7,7 +7,7 @@ class HabitsDatabase{
     String databasePath = await getDatabasesPath();
     String path = join(databasePath,"habits_db");
     Database database = await openDatabase(path,version: 1,onCreate: (Database db,int version)async{
-      await db.execute("CREATE TABLE Habits (id INTEGER PRIMARY KEY, name TEXT, amount TEXT, howManyWeeks INTEGER, time INTEGER, notifyId INTEGER)");
+      await db.execute("CREATE TABLE Habits (id INTEGER PRIMARY KEY, name TEXT, amount TEXT, time INTEGER, notifyId INTEGER)");
     });
     return database;
   }
